10853d6a7f45827132a6e2acf65c32bcf8fc310a
arwa-ayub <arwaayub07@gmail.com>
2026-04-17 01:57:30 +0500

    modified code

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from dotenv import load_dotenv
 import os
-
-load_dotenv()
-api_key = os.getenv("GROQ_API_KEY")
-
 import streamlit as st
 from groq import Groq
-import os
-from dotenv import load_dotenv
 import json
 import re
+
+load_dotenv()
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 # =============================
 # 🔐 LOAD ENV
